f9b189b8077646665148097f19c8d43ae72ff491
Alex Lalejini <amlalejini@gmail.com>
2020-04-30 11:17:05 -0400

    Update Aagos web code flow

--- source/web/Aagos-web.cc
+++ source/web/Aagos-web.cc
@@ -7,8 +7,35 @@
 #include "../AagosWeb.h"
 // #include "Evolve/NK.h"
 
-AagosConfig cfg;
-AagosWebInterface interface(cfg);
+struct AagosWebWrapper {
+  AagosConfig cfg;
+  emp::Ptr<AagosWebInterface> interface;
+
+  AagosWebWrapper() {
+    // Configure aagos
+    cfg.POP_SIZE(100);
+    cfg.MAX_GENS(1000);
+    cfg.SEED(2);
+    cfg.TOURNAMENT_SIZE(4);
+    cfg.GRADIENT_MODEL(true);
+    cfg.NUM_BITS(32);
+    cfg.NUM_GENES(4);
+    cfg.GENE_SIZE(4);
+    cfg.MAX_SIZE(128);
+    cfg.MIN_SIZE(4);
+    cfg.GENE_MOVE_PROB(0.003);
+    cfg.BIT_FLIP_PROB(0.003);
+    cfg.BIT_INS_PROB(0.001);
+    cfg.BIT_DEL_PROB(0.001);
+    interface = emp::NewPtr<AagosWebInterface>(cfg);
+  }
+
+  ~AagosWebWrapper() {
+    interface.Delete();
+  }
+};
+
+AagosWebWrapper aagos;
 
 int main() {
   std::cout << "Hello?" << std::endl;
