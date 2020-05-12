92350ec1d17b76b1c1d5568e96deba5fd9630153
Alex Lalejini <amlalejini@gmail.com>
2020-05-12 12:05:23 -0400

    Web interface config scaffolding in place

--- source/AagosWeb.h
+++ source/AagosWeb.h
@@ -6,10 +6,13 @@
  * - [ ] Allow world to be configured by web user
 */
 
+
 #include "web/web.h"
 #include "web/Input.h"
-
+#include "web/Element.h"
 #include "tools/string_utils.h"
+
+#include <unordered_map>
 
 #include "AagosWorld.h"
 #include "AagosConfig.h"
@@ -42,6 +45,8 @@ public:
 
   using config_t = AagosConfig;
   using canvas_draw_fun_t = std::function<void(void)>;
+  using input_callback_fun_t = std::function<void(std::string)>;
+  using input_checker_fun_t = std::function<bool(std::string)>;
 
   using org_t = AagosOrg;
   using genome_t = AagosOrg::Genome;
@@ -51,10 +56,15 @@ protected:
 
   UI::Document world_div;
   UI::Document control_div;
-  UI::Document config_div;
+  UI::Document config_general_div;
+  UI::Document config_genetic_arch_div;
+  UI::Document config_mutation_div;
+  UI::Document config_phase_2_evo_div;
 
   UI::Button run_toggle_but;
   UI::Button run_step_but;
+
+  std::unordered_map<std::string, UI::Input> config_input_elements;
 
   size_t draw_frequency=32;
 
@@ -63,12 +73,27 @@ protected:
   void RedrawPopulation(bool update_data=true);
   void RedrawEnvironment();
 
+  void SetupConfigInterface();
+
+  void DisableConfigInputs(bool in_dis=true) {
+    for (auto & cfg : config_input_elements) {
+      cfg.second.Disabled(in_dis);
+    }
+  }
+
+  bool CheckInputSize_t(const std::string & in) {
+    return (emp::is_digits(in) && in.size() && std::stoi(in) > 0);
+  }
+
 public:
   AagosWebInterface(config_t & cfg)
     : AagosWorld(cfg),
       world_div("emp_world_view"),
       control_div("emp_controls_view"),
-      config_div("emp_config_view"),
+      config_general_div("emp_view_config_general"),
+      config_genetic_arch_div("emp_view_config_genetic_architecture"),
+      config_mutation_div("emp_view_config_mutation"),
+      config_phase_2_evo_div("emp_view_config_phase_2_evolution"),
       pop_vis("emp-pop-vis")
   {
     std::cout << "AagowWebInterface constructor!" << std::endl;
@@ -90,9 +115,11 @@ void AagosWebInterface::SetupInterface() {
   run_toggle_but = UI::Button([this]() {
     ToggleActive();
     run_toggle_but.SetLabel(active ? "Stop" : "Run"); // TODO - use icons
-    active ? run_step_but.SetDisabled(true) : run_step_but.SetDisabled(false);
+    run_step_but.SetDisabled(active);
+    DisableConfigInputs(active);
   }, "Run", "run-toggle-button");
   run_toggle_but.SetAttr("class", "btn btn-block btn-lg btn-primary");
+  std::cout << "setup run_toggle_but" << std::endl;
 
   // Step button setup.
   run_step_but = GetStepButton("run-step-button");
@@ -118,18 +145,17 @@ void AagosWebInterface::SetupInterface() {
 
   control_div.Div("render-wrapper")
     << UI::Input([this](std::string in) {
-                  std::cout << "Change!" << std::endl;
                   const size_t val = std::stoul(in);
                   draw_frequency = val;
                  },
                  "number",
                  "",
                  "render-frequency")
-        .Checker([](std::string in) { return (emp::is_digits(in) && in.size() && std::stoi(in) > 0); })
+        .Checker([this](std::string in) { return CheckInputSize_t(in); })
         .Value((double)draw_frequency)
-        .Min("1")
-        .Max(emp::to_string(std::numeric_limits<size_t>::max()))
-        .Step("1")
+        .Min(1)
+        .Max(std::numeric_limits<size_t>::max())
+        .Step(1)
         .SetAttr("class", "form-control")
         .SetCSS("min-width", "96px");
 
@@ -138,8 +164,12 @@ void AagosWebInterface::SetupInterface() {
     << UI::Text().SetAttr("class", "input-group-text")
     << "th generation";
 
-  // ---- Setup world view interface ----
+  // ---- Setup config view interface ----
+  std::cout << "Setup config interface.."<< std::endl;
+  SetupConfigInterface();
+  std::cout << "done config interface setup"<< std::endl;
 
+  // ---- Setup world view interface ----
 
   // Setup world view.
   // - environment -
@@ -154,10 +184,6 @@ void AagosWebInterface::SetupInterface() {
     << UI::Div("pop-canvas-col").SetAttr("class", "col")
     << UI::Div("emp-pop-vis");
 
-  //   << pop_vis;
-    // << pop_canvas;
-
-  // pop_canvas_draw_fun = [this]() { DrawPopCanvas_FullPop(); };
 
   // Initial world configuration + pop canvas configuration.
   Setup(); // Call world setup
@@ -201,6 +227,64 @@ void AagosWebInterface::RedrawPopulation(bool update_data) {
 
 void AagosWebInterface::RedrawEnvironment() {
   pop_vis.DrawGradientEnv(*this, true);
+}
+
+void AagosWebInterface::SetupConfigInterface() {
+  config_general_div.Clear();
+
+  // --- Initialize relevant configuration options ---
+  // -- general --
+  config_input_elements["CHANGE_MAGNITUDE"]={[](std::string){ return true; }, "", ""};
+  auto & chg_magnitude_input = config_input_elements["CHANGE_MAGNITUDE"];
+  chg_magnitude_input.Type("number");
+  chg_magnitude_input.Label("");
+  chg_magnitude_input.Min(0);
+  chg_magnitude_input.Max(std::numeric_limits<size_t>::max());
+  chg_magnitude_input.Step(1);
+  chg_magnitude_input.Checker([this](std::string in) { return CheckInputSize_t(in); });
+  chg_magnitude_input.Callback([](std::string in) { std::cout << "CHG_MAG" << std::endl; });
+  chg_magnitude_input.Value(GetConfig().CHANGE_MAGNITUDE());
+
+  // config_input_elements["CHANGE_FREQUENCY"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["POP_SIZE"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["MAX_GENS"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["SEED"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["TOURNAMENT_SIZE"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["GRADIENT_MODEL"]={input_callback_fun_t(), "", ""};
+
+  // // // -- genetic architecture --
+  // config_input_elements["NUM_BITS_INPUT"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["NUM_GENES_INPUT"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["GENE_SIZE_INPUT"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["MAX_SIZE_INPUT"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["MIN_SIZE_INPUT"]={input_callback_fun_t(), "", ""};
+
+  // // // -- Mutations --
+  // config_input_elements["GENE_MOVE_PROB"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["BIT_FLIP_PROB"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["BIT_INS_PROB"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["BIT_DEL_PROB"]={input_callback_fun_t(), "", ""};
+
+  // // // -- phase 2 --
+  // config_input_elements["PHASE_2_ACTIVE"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["PHASE_2_CHANGE_MAGNITUDE"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["PHASE_2_CHANGE_FREQUENCY"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["PHASE_2_MAX_GENS"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["PHASE_2_TOURNAMENT_SIZE"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["PHASE_2_GENE_MOVE_PROB"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["PHASE_2_BIT_FLIP_PROB"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["PHASE_2_BIT_INS_PROB"]={input_callback_fun_t(), "", ""};
+  // config_input_elements["PHASE_2_BIT_DEL_PROB"]={input_callback_fun_t(), "", ""};
+
+  // -- Add inputs to interface --
+
+  config_general_div
+    << UI::Element("ul", "general-config-ul")
+        .SetAttr("class", "list-group list-group-flush")
+    << UI::Element("li", "change-magnitude-config-li")
+        .SetAttr("class", "list-group-item")
+    << config_input_elements["CHANGE_MAGNITUDE"];
+
 }
 
 #endif
