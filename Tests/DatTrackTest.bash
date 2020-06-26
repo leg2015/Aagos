a66d656f7ca07c16d277403672853f0a09c0fca9
Alex Lalejini <amlalejini@gmail.com>
2020-06-26 14:19:31 -0400

    remove old tests directory

--- Tests/DatTrackTest.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ..
-make clean
-make Aagos
-./Aagos -POP_SIZE 1 -NUM_BITS 10 -NUM_GENES 5 -GENE_SIZE 3 -MAX_GENS 10 -PRINT_INTERVAL 1 -STATISTICS_INTERVAL 1 -SNAPSHOT_INTERVAL 5 -SEED 6 > ./Tests/correct.txt
