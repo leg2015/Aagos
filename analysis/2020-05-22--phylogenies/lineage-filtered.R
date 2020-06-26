aabf537312e3d2f7da1f2179a003b8196d70c514
Alex Lalejini <amlalejini@gmail.com>
2020-06-26 12:40:32 -0400

    update lineage-filtered script, add compressed lineage data

--- analysis/2020-05-22--phylogenies/lineage-filtered.R
+++ analysis/2020-05-22--phylogenies/lineage-filtered.R
@@ -18,7 +18,7 @@ chg_rate_label <- function(mag, interval, drift) {
   else { return(paste(mag, interval, sep="/")) }
 }
 
-lineages <- fread("./data/filtered_lineage.csv")
+lineages <- fread("./data-lineages/filtered_lineage.csv")
 lineages$BIT_FLIP_PROB <- as.factor(lineages$BIT_FLIP_PROB)
 lineages$DRIFT <- lineages$TOURNAMENT_SIZE==1
 lineages$chg_rate_label <- factor(mapply(chg_rate_label, 
@@ -48,6 +48,9 @@ nk_lineages_BF03 <- filter(nk_lineages, BIT_FLIP_PROB==0.03)
 # Coding sites over time
 ####################################################################################
 
+# =========================
+# Gradient model, mutation rate 0.003
+# =========================
 g_bf003 <-
 ggplot(gradient_lineages_BF003, 
        aes(x=generation, y=coding_sites, fill=chg_rate_label, color=chg_rate_label)) +
@@ -59,6 +62,10 @@ ggplot(gradient_lineages_BF003,
                linetype=0) +
   ggtitle("Gradient Fitness Model") +
   ggsave("imgs/lineage_coding-sites_gradient_BF-0.003_full.png", width=20, height=10)
+
+# =========================
+# Gradient model, mutation rate 0.03
+# =========================
 
 g_bf03 <-
   ggplot(gradient_lineages_BF03, 
@@ -72,6 +79,10 @@ g_bf03 <-
   ggtitle("Gradient Fitness Model") +
   ggsave("imgs/lineage_coding-sites_gradient_BF-0.03_full.png", width=20, height=10)
 
+# =========================
+# NK fitness model, mutation rate 0.003
+# =========================
+
 nk_bf003 <-
   ggplot(nk_lineages_BF003, 
          aes(x=generation, y=coding_sites, fill=chg_rate_label, color=chg_rate_label)) +
@@ -83,6 +94,10 @@ nk_bf003 <-
                linetype=0) +
   ggtitle("NK Fitness Model") +
   ggsave("imgs/lineage_coding-sites_nk_BF-0.003_full.png", width=20, height=10)
+
+# =========================
+# NK fitness model, mutation rate 0.03
+# =========================
 
 nk_bf03 <-
   ggplot(nk_lineages_BF03, 
@@ -100,6 +115,9 @@ nk_bf03 <-
 # Genome Length Over Time
 ####################################################################################
 
+# =========================
+# Gradient model, mutation rate 0.003
+# =========================
 g_bf003 <-
   ggplot(gradient_lineages_BF003, 
          aes(x=generation, y=genome_length, fill=chg_rate_label, color=chg_rate_label)) +
@@ -113,10 +131,47 @@ g_bf003 <-
   ggsave("imgs/lineage_genome-length_gradient_BF-0.003_full.png", width=20, height=10)
 
 
-ggplot(filter(gradient_lineages_BF003, chg_rate_label=="drift"), 
-         aes(x=generation, y=coding_sites, fill=chg_rate_label, color=chg_rate_label)) +
-  geom_line() +
-  ggtitle("Gradient Fitness Model (drift only)") +
-  theme(legend.position = "none") +
-  ggsave("imgs/lineage_coding-sites_gradient_BF-0.003_full_drift-only.png", width=20, height=10)
+# =========================
+# Gradient model, mutation rate 0.03
+# =========================
+g_bf03 <-
+  ggplot(gradient_lineages_BF03, 
+         aes(x=generation, y=genome_length, fill=chg_rate_label, color=chg_rate_label)) +
+  stat_summary(fun.y="mean", geom="line") +
+  stat_summary(fun.data="mean_cl_boot", 
+               fun.args=list(conf.int=0.95), 
+               geom="ribbon", 
+               alpha=0.2,
+               linetype=0) +
+  ggtitle("Gradient Fitness Model") +
+  ggsave("imgs/lineage_genome-length_gradient_BF-0.03_full.png", width=20, height=10)
 
+# =========================
+# NK fitness model, mutation rate 0.003
+# =========================
+n_bf003 <-
+  ggplot(nk_lineages_BF003, 
+         aes(x=generation, y=genome_length, fill=chg_rate_label, color=chg_rate_label)) +
+  stat_summary(fun.y="mean", geom="line") +
+  stat_summary(fun.data="mean_cl_boot", 
+               fun.args=list(conf.int=0.95), 
+               geom="ribbon", 
+               alpha=0.2,
+               linetype=0) +
+  ggtitle("Gradient Fitness Model") +
+  ggsave("imgs/lineage_genome-length_nk_BF-0.003_full.png", width=20, height=10)
+
+# =========================
+# NK fitness model, mutation rate 0.03
+# =========================
+n_bf03 <-
+  ggplot(nk_lineages_BF03, 
+         aes(x=generation, y=genome_length, fill=chg_rate_label, color=chg_rate_label)) +
+  stat_summary(fun.y="mean", geom="line") +
+  stat_summary(fun.data="mean_cl_boot", 
+               fun.args=list(conf.int=0.95), 
+               geom="ribbon", 
+               alpha=0.2,
+               linetype=0) +
+  ggtitle("Gradient Fitness Model") +
+  ggsave("imgs/lineage_genome-length_nk_BF-0.03_full.png", width=20, height=10)
