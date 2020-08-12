a5d60caca52b532797f5df4d5d7324329296b4da
Alex Lalejini <amlalejini@gmail.com>
2020-08-12 19:52:50 -0400

    update aagos web defaults for beacon talk demo

--- source/web/Aagos-web.cc
+++ source/web/Aagos-web.cc
@@ -13,20 +13,20 @@ struct AagosWebWrapper {
 
   AagosWebWrapper() {
     // Configure aagos
-    cfg.POP_SIZE(100);
-    cfg.MAX_GENS(1000);
-    cfg.SEED(2);
+    cfg.POP_SIZE(500);  // original: 100
+    cfg.MAX_GENS(1500); // original: 1000
+    cfg.SEED(42);
     cfg.TOURNAMENT_SIZE(8);
     cfg.GRADIENT_MODEL(true);
     cfg.CHANGE_MAGNITUDE(1);
     cfg.CHANGE_FREQUENCY(8);
-    cfg.NUM_BITS(32);
-    cfg.NUM_GENES(4);
+    cfg.NUM_BITS(64);         // original: 32
+    cfg.NUM_GENES(8);         // original: 4
     cfg.GENE_SIZE(8);
     cfg.MIN_SIZE(8);
-    cfg.MAX_SIZE(128);
-    cfg.GENE_MOVE_PROB(0.003);
-    cfg.BIT_FLIP_PROB(0.003);
+    cfg.MAX_SIZE(512);        // original: 128
+    cfg.GENE_MOVE_PROB(0.001); // original: 0.003
+    cfg.BIT_FLIP_PROB(0.001);  // original: 0.003
     cfg.BIT_INS_PROB(0.001);
     cfg.BIT_DEL_PROB(0.001);
     cfg.PHASE_2_ACTIVE(false);
