4a0c364ff08b62a70d6cf952c2142390ac7322ec
Alex Lalejini <amlalejini@gmail.com>
2020-08-13 21:20:16 -0400

    update web app defaults for talk

--- source/web/Aagos-web.cc
+++ source/web/Aagos-web.cc
@@ -15,7 +15,7 @@ struct AagosWebWrapper {
     // Configure aagos
     cfg.POP_SIZE(500);  // original: 100
     cfg.MAX_GENS(1000); // original: 1000
-    cfg.SEED(32);
+    cfg.SEED(8);
     cfg.TOURNAMENT_SIZE(8);
     cfg.GRADIENT_MODEL(true);
     cfg.CHANGE_MAGNITUDE(1);
